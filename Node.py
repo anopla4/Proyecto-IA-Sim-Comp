4136d06da79da79b0c7b538f5dad0845c7e5d20b
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2021-12-18 17:37:03 -0500

    fix: Node children list and add action

--- Node.py
+++ Node.py
@@ -1,9 +1,11 @@
 from State import State
+from Action import Action
 
 class Node(object):
-    def __init__(self,state : State, parent : Node = None):
+    def __init__(self,state : State, parent : Node = None, action : Action = None):
         self.state = state
         self.parent = parent
-        self.children = list[Node]
+        self.children = []
         self.visit_count = 0
         self.utility_score = 0
+        self.action = action
