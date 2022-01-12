aa8c70488f01d275e8f69a8befefa761b1738791
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-01-11 21:28:19 -0800

    refactor: Build cst code

--- Language/Lexer/RegularExpression/cst.py
+++ Language/Lexer/RegularExpression/cst.py
@@ -22,42 +22,29 @@ def build_cst(ll_table, s, symbol):
         node = stack.pop(-1)
         if i == len(s):
             node = stack.pop(-1)
+            p = None
             for terminal in ll_table[node.value]:
                 production = ll_table[node.value][terminal]
                 if production and isinstance(production.right_side[0], Epsilon):
                     p = production
                     break
-            new_child = TreeNode(p.right_side[0], [])
-            node.children.append(new_child)
-
-        elif node.value.symbol == ")":
-            i += 1
+            if p != None:
+                new_child = TreeNode(p.right_side[0], [])
+                node.children.append(new_child)
         else:
             p = [
                 ll_table[node.value][terminal]
                 for terminal in ll_table[node.value]
                 if s[i] == terminal.symbol and ll_table[node.value][terminal] != None
-            ]
-            if not p:
-                for terminal in ll_table[node.value]:
-                    production = ll_table[node.value][terminal]
-                    if production and isinstance(production.right_side[0], Epsilon):
-                        p = [production]
-                        break
-                if not p:
-                    raise Exception("Terminal not found.")
-            else:
-                children = list(reversed(p[0].right_side))
-                for j in range(len(children)):
-                    new_child = TreeNode(children[j], [])
-                    node.children.append(new_child)
-                    if isinstance(children[j], Terminal) and s[i] == children[j].symbol:
-                        i += 1
-                    elif (
-                        not isinstance(children[j], Epsilon)
-                        and children[j].symbol != ")"
-                    ):
-                        stack.append(new_child)
+            ][0]
+            children = list(reversed(p.right_side))
+            for j in range(len(children)):
+                new_child = TreeNode(children[j], [])
+                node.children.append(new_child)
+                if isinstance(children[j], Terminal) and s[i] == children[j].symbol:
+                    i += 1
+                elif not isinstance(children[j], Epsilon) and children[j].symbol != ")":
+                    stack.append(new_child)
         print(f"{node.value.symbol} -> {[a.value.symbol for a in node.children]}")
 
     return cst
