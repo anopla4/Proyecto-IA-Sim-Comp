60cf9b51c99d5a074c4abbba2ae25ca62c841e9d
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-01-29 18:41:17 -0500

    add: attribute production

--- Language/Parsing/production.py
+++ Language/Parsing/production.py
@@ -1,4 +1,5 @@
 class Production:
-    def __init__(self, left_side, right_side) -> None:
+    def __init__(self, left_side, right_side, attributes = []) -> None:
         self.left_side = left_side
         self.right_side = right_side
+        self.attributes = attributes
