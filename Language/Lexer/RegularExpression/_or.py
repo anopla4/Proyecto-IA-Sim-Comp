a21407f8edb4d13f02558ded5b1da0552876010d
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-01-29 20:34:33 +0100

    feat: Build regular expression automaton from ast

--- Language/Lexer/RegularExpression/_or.py
+++ Language/Lexer/RegularExpression/_or.py
@@ -1,6 +1,28 @@
+from Language.Automaton.state import State
+from Language.Automaton.automaton import Automaton
 from .binary_expression import BinaryExpression
 
 
 class Or(BinaryExpression):
     def __init__(self, left_expr, right_expr) -> None:
         super().__init__("|", 3, left_expr, right_expr)
+
+    def build_automaton(self, left_automaton, right_automaton):
+        init_state = State()
+        final_state = State()
+        states = [init_state, final_state] + left_automaton.states + right_automaton.states
+        characters = ["epsilon"] + left_automaton.characters + right_automaton.characters
+        characters = list(set(characters))
+        transition_function = dict(left_automaton.transition_function.items() + right_automaton.transition_function.items())
+        initial_state_left = left_automaton.initial_state
+        final_state_left = left_automaton.f
+        initial_state_right = right_automaton.initial_state
+        final_state_right = right_automaton.f
+        init_state.next_states = [initial_state_left, initial_state_right]
+        transition_function[(init_state, "epsilon")] = [initial_state_left, initial_state_right]
+        final_state_left.next_states.append(final_state)
+        transition_function[(final_state_left, "epsilon")] = [final_state]
+        final_state_right.next_states.append(final_state)
+        transition_function[(final_state_right, "epsilon")] = [final_state]
+
+        return Automaton(states, init_state, characters, final_state, transition_function)
--- Language/Lexer/RegularExpression/word.py
+++ Language/Lexer/RegularExpression/word.py
@@ -1,6 +1,23 @@
+from Language.Automaton.state import State
+from Language.Automaton.automaton import Automaton
 from ..expression import Expression
 
 
 class Word(Expression):
     def __init__(self, w) -> None:
         super().__init__(w, 1)
+
+    def build_automaton(self):
+        initial_state = State()
+        last_state = initial_state
+        characters = list(self.regex)
+        transition_function = {}
+        automaton = Automaton()
+        states = [last_state]
+        for i in self.regex:
+            st = State()
+            states.append(st)
+            transition_function[(last_state, i)] = st
+            last_state = st
+        return Automaton(states, initial_state, characters, last_state, transition_function)
+
