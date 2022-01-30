76f1e79ce3f80becdcedd27ca30eebd44f728525
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-01-29 19:07:14 -0500

    fix: rules

--- Language/Parsing/Grammar.py
+++ Language/Parsing/Grammar.py
@@ -2,10 +2,11 @@ from epsilon import Epsilon
 from dollar import Dollar
 
 class Grammar:
-    def __init__(self, attributes):
-        self.non_terminals = []
-        self.terminals = []
-        self.productions = []
-        self.start_symbol = None
+    def __init__(self, attributes, non_terminals = [], terminals = [], productions = [], start_symbol = None, productions_attributes = {}):
+        self.non_terminals = non_terminals
+        self.terminals = terminals
+        self.productions = productions
+        self.start_symbol = start_symbol
         self.epsilon = Epsilon()
         self.EOF = Dollar()
+        self.rules = productions_attributes
