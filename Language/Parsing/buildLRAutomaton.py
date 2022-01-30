1f3779492b6a6c86af8beaf07ee453a76747e869
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-01-29 23:34:23 -0500

    feat: build LR0 automaton

---
+++ Language/Parsing/buildLRAutomaton.py
@@ -0,0 +1,36 @@
+from Item import Item
+
+def build_lr0_automaton(self,G):
+    assert len([production for production in G.productions if production.left_side == G.start_symbol]) == 1, "The grammar must be augmented"
+
+    states = set()
+    transitions = {}
+    characters = set()
+    characters.add('epsilon')
+    initial_state = Item([production for production in G.productions if production.left_side == G.start_symbol][0],0)
+
+    stack = [initial_state,]
+
+    while stack:
+        current_state = stack.pop()
+
+        states.add(current_state)
+
+        next_symbol = current_state.NextSymbol
+        characters.add(next_symbol)
+        next_state = current_state.NextItem()
+
+        if (current_state, next_symbol) not in transitions:
+            transitions[(current_state, next_symbol)] = []
+        transitions[(current_state, next_symbol)].append(next_state)
+        stack.append(next_state)
+
+        if isinstance(next_symbol, NonTerminal):
+            for production in G.productions:
+                if production.left_side == next_symbol:
+                    if (state,'epsilon') not in transitions:
+                        transitions[(state, 'epsilon')] = []
+                    next_state = Item(production,0)
+                    transitions[(state, 'epsilon')].append(next_state)    
+                    stack.append(next_state)
+    return list(states),initial_state,characters,list(states),transitions
