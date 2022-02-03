98f56b4347fc364aae686ba8466a04566e7cee02
AlejBV <alejandro.beltran@estudiantes.matcom.uh.cu>
2022-02-02 21:21:33 -0500

    feat/fuzzy functions

---
+++ Simulation/FuzzyEngine/fuzzy_functions.py
@@ -0,0 +1,147 @@
+
+from scipy import integrate
+import pytest
+
+def inference_by_mandami(param,func):
+    """
+ funcion para obtener la inferencia usando el metodo de Mandami
+ param es el valor de corte y func es la funcion a la que se le hara el
+ el corte. Devuelve una funcion que es el resultado de aplicar el metodo de
+ Mandami en parametro
+    """
+    def inner(*args):
+        return min(param,func(*args))
+    return inner
+
+# funcion para agrupar las salidas del fuzzy -> metodo Max
+def max_agreg(list_functions):
+    """
+    Recibe una lista de funciones, y se devuelve una funcion que recibe que
+    un parametro y devuelve el mayor de todos los f(x) para todas las funciones
+    en la lista para un mismo x. Se espera que todas las funciones hayan 
+    sido previamente obtenidas por el metodo de Mandami
+    """
+    def inner(*args):
+        max_val=0
+        for i in list_functions:
+            max_val=max(max_val,i(*args))
+        return max_val
+    return inner
+
+def discret_centroid(func,lower_range,upper_range,step=1):
+    """
+    Funcion hecha para defuzzificar un valor aplicando el metodo de los 
+    centroides en una variable con dominio discreto
+    """
+    num=0
+    den=0
+    for i in range(lower_range,upper_range,step):
+        num+=i*func(i)
+        den+=func(i)
+    return num/den
+
+
+def __defuzzing_by_centroid(agregation_function,lower_range:float,upper_range:float):
+    """
+    Funcion hecha para defuzzificar un valor aplicando el metodo de los 
+    centroides en una variable con dominio continuo
+    """
+    func= lambda f: lambda x: x*f(x)
+    num=integrate.quad(func(agregation_function),lower_range,upper_range)
+    den=integrate.quad(agregation_function,lower,upper_range)
+    return num[0]/den[0]
+
+# funciones fuzzy basicas:
+
+def singleton(x0):
+    """
+    Funcion singleton, recibe un parametro y devuelve una instancia de la
+    funcion singleton con el valor fijado a x0
+    """
+    def inner(x):
+        if x==x0:
+            return 1.0
+    
+        else :
+            return 0
+    return inner
+
+def triang_shape(params):
+    """
+    Funcion triangular , recibe como parametro una lista [a,b,c] con todos
+    valores reales que representan las distintas regiones de la funcion.
+    Devuelve una funcion instanciada para un conjunto de parametros 
+    especificos
+    """
+    a=params[0]
+    b=params[1]
+    c=params[2]
+    assert a<=b<=c
+    def inner(x):
+        result=0
+        if x<=a:
+            result=0
+        if a<x<=b:
+            result=(x-a)/(b-a)
+    
+        if b<x<c:
+            result=(c-x)/(c-b)
+    
+        if x>=c:
+            resutl=1
+        return result
+    return inner
+            
+def s_shape(params):
+    """
+    Funcion S, recibe como parametro una lista [a,b,c] con todos
+    valores reales que representan las distintas regiones de la funcion.
+    Devuelve una funcion instanciada para un conjunto de parametros 
+    especificos
+    """
+    a=params[0]
+    b=params[1]
+    c=params[2]
+    assert a<=b<=c
+    def inner(x):
+        result=0
+        if x<=a:
+            result=0
+        if a<x<=b:
+            result=2*((x-a)/(c-a))*((x-a)/(c-a))
+    
+        if b<x<c:
+            result=1-2*((x-a)/(c-a))*((x-a)/(c-a))
+    
+        if x>=c:
+            resutl=1
+        return result
+    return inner
+
+def trapezoidal_shape(params):
+    """
+    Funcion trapezoidal , recibe como parametro una lista [a,b,c,d] con todos
+    valores reales que representan las distintas regiones de la funcion.
+    Devuelve una funcion instanciada para un conjunto de parametros 
+    especificos
+    """
+    a=params[0]
+    b=params[1]
+    c=params[2]
+    d=params[3]
+    assert a<=b<=c<=d
+    def inner(x):
+        result=0.0
+    
+        if x<=a or x>=d:
+            result=0.0
+        
+        if a<x<=b:
+            result=(x-a)/(b-a)
+    
+        if b<x<c:
+            result=1.0
+        if c<x<d:
+            result=(d-x)/(d-c)
+        return result
+    return inner
---
+++ fuzzy/fuzzy_functions_test.py
@@ -0,0 +1,37 @@
+from fuzzy_functions import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+A continuacion se presentan 3 funciones triangulares que reprentan los
+3 conjuntos difusos de una variable arbitraria, cada una de estas funciones
+se instanciaron con parametros distintos y se les aplico el metodos de mandami
+con valores de corte distintos. Este ejemplo de prueba esta basado en 
+el sistem difuso visto en Conferencia
+"""
+
+#Primera funcion
+trian1=triang_shape([5,10,15])
+f1=inference_by_mandami(0.15,trian1)
+
+#Segunda funcion
+trian2=triang_shape([10,15,20])
+f2=inference_by_mandami(0.7, trian2)
+
+#Tercera funcion
+trian3=triang_shape([15,20,25])
+f3=inference_by_mandami(0.8, trian3)
+
+# Lista con todas las funciones  
+func_list=[f1,f2,f3]
+""" 
+funcion que se obtiene luego de aplicar el metodo de agregacion  por maximo
+a las 3 funciones que previamente se les aplico mandami
+"""
+current_func=max_agreg(func_list)
+
+x=np.linspace(5, 25)
+for i in x:
+    plt.scatter(i,current_func(i))
+plt.show()
+print(defuzzing_by_centroid(current_func, 5, 25))
