d575a935031219c616078a21f636a1e3727db29c
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-02-04 10:50:40 -0500

    fix: type property in FinalState

--- Language/Automaton/final_state.py
+++ Language/Automaton/final_state.py
@@ -2,6 +2,6 @@ from .State import State
 
 
 class FinalState(State):
-    def __init__(self, state = None, t = None):
+    def __init__(self, state = None, t = []):
         super().__init__(state)
         self.type = t
