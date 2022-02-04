0d5cd597afaa181e4944d2366941416247d9526e
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-02-04 09:40:41 -0500

    fix: shift reduce parser problems with Grammar instance and States in automaton

--- Language/Parsing/build_ast_shift_reduce_parser.py
+++ Language/Parsing/build_ast_shift_reduce_parser.py
@@ -1,5 +1,4 @@
-from shift_reduce_parser import ShiftReduceParser
-from Lenguage.Lexer.RegularExpression.rule import ProductionRules
+from .shift_reduce_parser import ShiftReduceParser
 
 
 def build_ast(actions, productions, tokens, G):
@@ -8,7 +7,7 @@ def build_ast(actions, productions, tokens, G):
     
     stack = []
 
-    producitons = iter(productions)
+    productions = iter(productions)
     tokens = iter(tokens)
 
     for action in actions:
@@ -16,16 +15,16 @@ def build_ast(actions, productions, tokens, G):
             stack.append(next(tokens))
         elif action == ShiftReduceParser.REDUCE:
             production = next(productions)
-            tokens_production = [None] + stakc[-len(production.right_side):]
-            rule = G.rules[production][0] #sinthetyzed rule
-
+            tokens_production = [None] + stack[-len(production.right_side):]
+            rule = G.rules[production] #synthesized rule
+            l = []
+            l.append(object)
             if len(production.right_side):
                 value = rule(None, tokens_production)
-                stack[-len(production.right_side):] = value
+                stack[-len(production.right_side):] = [value]
             else:
                 stack.append(rule(None, None))
-
-    if not isinstance(next(token), type(G.EOF)):
+    if not next(tokens).type == G.EOF:
         raise "Cadena invalida"
     return stack[0]
 
