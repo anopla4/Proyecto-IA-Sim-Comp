227e77f0c20b2176ecd93d7fa0cf26148d318699
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-02-04 09:40:41 -0500

    fix: shift reduce parser problems with Grammar instance and States in automaton

--- Language/Parsing/shift_reduce_parser.py
+++ Language/Parsing/shift_reduce_parser.py
@@ -1,9 +1,10 @@
-from Grammar import Grammar
-from buildLRAutomaton import build_lr0_automaton
-from first import first
-from follow import follow
-from non_terminal import NonTerminal
-from terminal import Terminal
+from Language.Parsing.production import Production
+from .Grammar import Grammar
+from .buildLRAutomaton import build_lr0_automaton
+from .first import first
+from .follow import follow
+from .non_terminal import NonTerminal
+from .terminal import Terminal
 
 class ShiftReduceParser:
     SHIFT = "Shift"
@@ -17,23 +18,28 @@ class ShiftReduceParser:
         self.build_parser_table()
     
     def build_parser_table(self):
+        G = self.G
         Augmented_G = Grammar.augment(G)
         automaton = build_lr0_automaton(Augmented_G)
-
-        firsts = first(G.characters,G.productions, G.terminals)
-        follows = follow(G.start_symbol, G.characters, G.productions, firsts)
+        symbols = list(Augmented_G.terminals)
+        symbols.extend(Augmented_G.non_terminals)
+        
+        firsts = first(symbols ,Augmented_G.productions, Augmented_G.terminals)
+        follows = follow(Augmented_G.start_symbol, symbols, Augmented_G.productions, firsts, True, G.EOF)
 
         for i,state in enumerate(automaton.q):
-            for item in state:
+            for item in state.state:
+                item = item.state
                 if item.IsReduceItem:
-                    if item.production.left_side == G.start_symbol:
+                    if item.production.left_side == Augmented_G.start_symbol:
                         assert (i,G.EOF) not in self.action or self.action[(i,G.EOF)] == (ShiftReduceParser.OK, None), 'Shift-Reduce or Reduce-Reduce conflict'
                         self.action[(i,G.EOF)] = (ShiftReduceParser.OK, None)
-                    for symbol in follow[item.production.left_side]:
-                        assert (i,symbol) not in self.action or self.action[(i,symbol)] == (ShiftReduceParser.REDUCE, item.production), 'Shift-Reduce or Reduce-Reduce conflict'
-                        self.action[(i,symbol)] = (ShiftReduceParser.REDUCE, item.production)
+                    else:
+                        for symbol in follows[item.production.left_side]:
+                            assert (i,symbol) not in self.action or self.action[(i,symbol)] == (ShiftReduceParser.REDUCE, item.production), 'Shift-Reduce or Reduce-Reduce conflict'
+                            self.action[(i,symbol)] = (ShiftReduceParser.REDUCE, item.production)
                 else:
-                    next_symbol = item.next_symbol
+                    next_symbol = item.NextSymbol
                     next_state_index = automaton.q.index(automaton.transition_function[(state,next_symbol)])
                     if isinstance(next_symbol, Terminal):
                         assert (i,next_symbol) not in self.action or self.action[(i,next_symbol)] == (ShiftReduceParser.SHIFT, next_state_index), 'Shift-Reduce conflict!!!'
@@ -41,7 +47,7 @@ class ShiftReduceParser:
                     else:
                         assert (i,next_symbol) not in self.goto or self.goto[(i,next_symbol)] == next_state_index, 'Shift-Reduce or Reduce-Reduce conflict!!!'
                         self.goto[(i,next_symbol)] = next_state_index
-
+        
     def __call__(self, w):
         stack = [0,]
         result = []
@@ -60,7 +66,7 @@ class ShiftReduceParser:
             if action ==  ShiftReduceParser.SHIFT:
                 stack.append(tag)
                 actions.append(action)
-                cursor += 1
+                index += 1
             elif action == ShiftReduceParser.REDUCE:
                 for _ in range(len(tag.right_side)):
                     stack.pop()
