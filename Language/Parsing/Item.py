0d06396d2e197fb1912fd1cc02d34da084033e5d
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-02-04 22:40:17 -0500

    add: lockahead to Items

--- Language/Parsing/Item.py
+++ Language/Parsing/Item.py
@@ -1,11 +1,11 @@
 class Item:
-    def __init__(self,production,pos):
+    def __init__(self,production,pos, lockahead = set()):
         self.production = production
         self.pos = pos
+        self.lockahead = lockahead
     
     @property
     def NextSymbol(self):
-        # print(self.production.right_side.symbol)
         if self.pos < len(self.production.right_side):
             return self.production.right_side[self.pos]
         return None
@@ -22,7 +22,10 @@ class Item:
         return self.production.left_side.symbol + '->' + ''.join([symbol.symbol for symbol in self.production.right_side]) + str(self.pos)
     
     def __eq__(self, other):
-        return self.production == other.production and self.pos == other.pos
+        return self.production == other.production and self.pos == other.pos and self.lockahead == other.lockahead
     
     def __hash__(self):
-        return hash((self.production,self.pos))
+        return hash((self.production,self.pos,self.lockahead))
+    
+    def Center(self):
+        return Item(self.production, self.pos)
