0b976fa7d1f4d14a3397ab21f8aa7ea08f339a2b
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-02-06 15:39:42 -0500

    fix: import problems

--- Language/Lexer/RegularExpression/_or.py
+++ Language/Lexer/RegularExpression/_or.py
@@ -1,4 +1,4 @@
-from Language.Automaton.state import State
+from Language.Automaton.State import State
 from Language.Automaton.automaton import Automaton
 from .binary_expression import BinaryExpression
 
@@ -10,22 +10,30 @@ class Or(BinaryExpression):
     def build_automaton(self, left_automaton, right_automaton):
         init_state = State()
         final_state = State()
-        states = [init_state, final_state] + \
-            left_automaton.states + right_automaton.states
-        characters = ["epsilon"] + left_automaton.characters + \
-            right_automaton.characters
+        states = (
+            [init_state, final_state] + left_automaton.states + right_automaton.states
+        )
+        characters = (
+            ["epsilon"] + left_automaton.characters + right_automaton.characters
+        )
         characters = list(set(characters))
-        transition_function = dict(list(left_automaton.transition_function.items(
-        )) + list(right_automaton.transition_function.items()))
+        transition_function = dict(
+            list(left_automaton.transition_function.items())
+            + list(right_automaton.transition_function.items())
+        )
         initial_state_left = left_automaton.initial_state
         final_states_left = left_automaton.f
         initial_state_right = right_automaton.initial_state
         final_states_right = right_automaton.f
         transition_function[(init_state, "epsilon")] = [
-            initial_state_left, initial_state_right]
+            initial_state_left,
+            initial_state_right,
+        ]
         for fs in final_states_left:
             transition_function[(fs, "epsilon")] = [final_state]
         for fs in final_states_right:
             transition_function[(fs, "epsilon")] = [final_state]
 
-        return Automaton(states, init_state, characters, [final_state], transition_function)
+        return Automaton(
+            states, init_state, characters, [final_state], transition_function
+        )
