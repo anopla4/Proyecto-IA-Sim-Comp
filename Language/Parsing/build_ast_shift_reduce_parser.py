80a59dbb2012570f91a4037e39eb959242cbebdd
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-02-06 15:50:50 -0500

    fix: problems with epsilon transitions in parser

--- Language/Parsing/build_ast_shift_reduce_parser.py
+++ Language/Parsing/build_ast_shift_reduce_parser.py
@@ -4,7 +4,7 @@ from .shift_reduce_parser import ShiftReduceParser
 def build_ast(actions, productions, tokens, G):
     if not actions or not productions or not tokens:
         return None
-    
+
     stack = []
 
     productions = iter(productions)
@@ -15,19 +15,18 @@ def build_ast(actions, productions, tokens, G):
             stack.append(next(tokens))
         elif action == ShiftReduceParser.REDUCE:
             production = next(productions)
-            tokens_production = [None] + stack[-len(production.right_side):]
-            rule = G.rules[production] #synthesized rule
+            flag = False
+            if production.right_side[0] != G.epsilon:
+                tokens_production = [None] + stack[-len(production.right_side) :]
+                flag = True
+            rule = G.rules[production]  # synthesized rule
             l = []
             l.append(object)
-            if len(production.right_side):
+            if len(production.right_side) and flag:
                 value = rule(None, tokens_production)
-                stack[-len(production.right_side):] = [value]
+                stack[-len(production.right_side) :] = [value]
             else:
                 stack.append(rule(None, None))
-    if not next(tokens).type == G.EOF:
+    if not next(tokens).type[0] == G.EOF:
         raise "Cadena invalida"
     return stack[0]
-
-
-    
-
