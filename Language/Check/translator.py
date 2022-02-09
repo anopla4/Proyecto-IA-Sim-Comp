ae0848e9e12121416a9ba9e6f147f2e1f1f90d74
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-02-09 13:57:10 -0500

    feat: DSL translator

--- Language/Check/translator.py
+++ Language/Check/translator.py
@@ -1,4 +1,4 @@
-import visitor
+from . import visitor
 from ..Parsing.Node.declaration_nodes import *
 from ..Parsing.Node.expression_nodes import *
 from ..Parsing.Node.statement_nodes import *
@@ -8,7 +8,7 @@ from .error import *
 from .scope import Scope
 
 
-class FormatVisitor(object):
+class Translator(object):
     @visitor.on("node")
     def visit(self, node, tabs):
         pass
@@ -16,12 +16,17 @@ class FormatVisitor(object):
     @visitor.when(ProgramNode)
     def visit(self, node, tabs=0):
         statements = "\n".join(self.visit(child, tabs) for child in node.statements)
-        return f"{statements}"
+        import1 = "from Simulation.environment import Environment"
+        import2 = "from Simulation.parameter import Parameter"
+        import3 = "from Simulation.activation_rule import ActivationRule"
+        import4 = "from Simulation.symptom import Symptom"
+        import5 = "from Simulation.intervention import Intervention"
+        return f"{import1}\n{import2}\n{import3}\n{import4}\n{import5}\n{statements}"
 
     @visitor.when(ClassNode)
     def visit(self, node, tabs=0):
         parent = "" if node.parent is None else f"({node.parent})"
-        ans = "\t" * tabs + f"class {node.id} {parent}: {{ {node.statements} }}"
+        ans = "\t" * tabs + f"class {node.id}{parent}:"
         features = "\n".join(self.visit(child, tabs + 1) for child in node.statements)
         return f"{ans}\n{features}"
 
@@ -47,7 +52,7 @@ class FormatVisitor(object):
     def visit(self, node, tabs=0):
         params = ", ".join(param for param in node.params_names)
         body = "\n".join(self.visit(child, tabs + 1) for child in node.body)
-        ans = "\t" * tabs + f"def {node.id}({params}): {body}"
+        ans = "\t" * tabs + f"def {node.id}({params}):\n{body}"
         return ans
 
     @visitor.when(BinaryNode)
@@ -60,7 +65,7 @@ class FormatVisitor(object):
     @visitor.when(UnaryNode)
     def visit(self, node, tabs=0):
         expr = self.visit(node.expr)
-        ans = "\t" * tabs + f"{node.symbol} {expr}"
+        ans = "\t" * tabs + f"{node.symbol} ({expr})"
         return ans
 
     @visitor.when(VariableNode)
@@ -71,10 +76,14 @@ class FormatVisitor(object):
     def visit(self, node, tabs=0):
         return "\t" * tabs + f"{node.lex}"
 
+    @visitor.when(StringNode)
+    def visit(self, node, tabs=0):
+        return "\t" * tabs + f'"{node.lex}"'
+
     @visitor.when(CallNode)
     def visit(self, node, tabs=0):
         obj = f"{self.visit(node.obj)}." if node.obj != None else ""
-        args = ",".join(arg for arg in node.arguments)
+        args = ",".join(self.visit(arg) for arg in node.arguments)
         ans = "\t" * tabs + f"{obj}{node.lex}({args})"
         return ans
 
@@ -85,30 +94,41 @@ class FormatVisitor(object):
 
     @visitor.when(RuleNode)
     def visit(self, node, tabs=0):
-        expr_l = self.visit(node.left)
-        expr_r = self.visit(node.right)
-        return "\t" * tabs + f"if {expr_l}: \n\t {expr_r}"
+        condition = ",".join([self.visit(cond) for cond in node.condition])
+        dest = self.visit(node.destination)
+        then_var = self.visit(node.then_var)
+        then_val = self.visit(node.then_val)
+        return (
+            "\t" * tabs
+            + f"Rule({{{condition}}}, destination={dest}, then=({then_var}, {then_val}))"
+        )
 
     @visitor.when(ProbFunctionValueNode)
     def visit(self, node, tabs=0):
-        return "\t" * tabs + f"if p < {node.num}: \n\t return {node.val}"
+        num = "\t" * tabs + f"if p <= {self.visit(node.num)}:"
+        value = "\t" * (tabs + 1) + f"return {self.visit(node.val)}"
+        return f"{num}\n{value}"
 
     @visitor.when(ProbabilityFunctionNode)
     def visit(self, node, tabs=0):
         acc = 0
         args = []
         for v in node.values:
-            v.num = str(acc + int(v.num))
-            args.append(self.visitor(v, tabs + 1))
+            num = self.visit(v.num)
+            acc += float(num)
+            v.num = ConstantNumNode(str(acc))
+            args.append(self.visit(v, tabs + 1))
         args = "\n".join(args)
-        return "\t" * tabs + f"def f(): \n  {args}"
+        return "\t" * tabs + f"def f(p): \n  {args}"
 
     @visitor.when(EffectNode)
     def visit(self, node, tabs=0):
-        return (
-            "\t" * tabs
-            + f"if {node.env}.get_parameter({node.par}): \n {node.env}.update_parameter({node.par, node.e})"
-        )
+        param = self.visit(node.par)
+        env = self.visit(node.env)
+        element = self.visit(node.e)
+        condition = "\t" * tabs + f"if {env}.get_parameter({param}):"
+        body = "\t" * (tabs + 1) + f"{env}.update_parameter({param}, {element})"
+        return f"{condition}\n{body}"
 
     @visitor.when(EffectRuleNode)
     def visit(self, node, tabs=0):
@@ -116,10 +136,10 @@ class FormatVisitor(object):
 
     @visitor.when(AgentDefNode)
     def visit(self, node, tabs=0):
-        conditions = self.visitor(node.conditions)
+        conditions = self.visit(node.conditions)
         ans = (
             "\t" * tabs
-            + f"{node.idt}({node.idn}, {conditions}, {node.t}, {node.repetition}, {node.action}"
+            + f"{node.idt}({node.idn}, {conditions}, {node.t}, {node.rep}, f"
         )
         if node.supply != None:
             ans += f"{node.supply})\n"
@@ -133,21 +153,45 @@ class FormatVisitor(object):
 
     @visitor.when(ForNode)
     def visit(self, node, tabs=0):
-        stms = "\n".join([self.visitor(arg, tabs + 1) for arg in node.body])
-        ans = "\t" * tabs + f"for {node.var} in {node.expr}: \n {stms}"
+        stms = "\n".join([self.visit(arg, tabs + 1) for arg in node.body])
+        var = self.visit(node.var)
+        expr = self.visit(node.expr)
+        ans = "\t" * tabs + f"for {var} in {expr}: \n {stms}"
         return ans
 
     @visitor.when(IfNode)
     def visit(self, node, tabs=0):
-        expr = self.visitor(node.expr)
-        stms = "\n".join([self.visitor(arg, tabs + 1) for arg in node.body])
+        expr = self.visit(node.expr)
+        stms = "\n".join([self.visit(arg, tabs + 1) for arg in node.body])
         ans = "\t" * tabs + f"if {expr}: \n {stms}"
         return ans
 
     @visitor.when(IfElseNode)
     def visit(self, node, tabs=0):
-        expr = self.visitor(node.expr)
-        stms_if = "\n".join([self.visitor(arg, tabs + 1) for arg in node.body_if])
-        stms_else = "\n".join([self.visitor(arg, tabs + 1) for arg in node.body_else])
-        ans = "\t" * tabs + f"if {expr}: \n {stms_if} \n else: \n {stms_else}"
-        return ans
+        expr = self.visit(node.condition_if)
+        stms_if = "\n".join([self.visit(arg, tabs + 1) for arg in node.body_if])
+        stms_else = "\n".join([self.visit(arg, tabs + 1) for arg in node.body_else])
+        ans_if = "\t" * tabs + f"if {expr}: \n {stms_if}"
+        ans_else = "\t" * tabs + f"else: \n {stms_else}"
+        return f"{ans_if}\n{ans_else}"
+
+    @visitor.when(ItemNode)
+    def visit(self, node, tabs=0):
+        key = self.visit(node.key)
+        value = self.visit(node.val)
+        return "\t" * tabs + f"{key}: {value}"
+
+    @visitor.when(DictNode)
+    def visit(self, node, tabs=0):
+        items = ", ".join([self.visit(i) for i in node.items])
+        return "\t" * tabs + f"{{{items}}}"
+
+    @visitor.when(ListNode)
+    def visit(self, node, tabs=0):
+        items = ", ".join([self.visit(i) for i in node.items])
+        return "\t" * tabs + f"[{items}]"
+
+    @visitor.when(TupleNode)
+    def visit(self, node, tabs=0):
+        items = ", ".join([self.visit(i) for i in node.items])
+        return "\t" * tabs + f"({items})"
