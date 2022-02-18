79189eeba47e73c316709561e2c7d6df853483ac
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-02-17 20:22:58 -0500

    add: intervention to test case

--- main.py
+++ main.py
@@ -55,13 +55,25 @@ main(
     """Parameter p_plaqueta = new Parameter("plaqueta", 18, 25, 50, -20, 80); \n
     Parameter p_tos= new Parameter("tos",10, 0, 5,-20,60);\n
     Parameter p_dolorCabeza= new Parameter("dolor de cabeza",26, 0, 15,-20,100);\n
-    Parameter fiebre = new Parameter("fiebre", 36.5, 35, 36.9, 34.5, 43); \n
+    Parameter p_fiebre = new Parameter("fiebre", 36.5, 35, 36.9, 34.5, 43); \n
     Patient p = new Patient([p_plaqueta]); \n 
-    RandVarEffect fiebre_action = { 0.3 -> effect fiebre p 1; 0.45 ->  effect fiebre p 1.4; 0.25 -> effect fiebre p 1.8}; \n
-    RandVarEffect tos_action = { 0.3 -> effect fiebre p 1; 0.45 ->  effect fiebre p 1.4; 0.25 -> effect fiebre p 1.8}; \n
-    RandVarEffect dc_action = { 0.3 -> effect fiebre p 1; 0.45 ->  effect fiebre p 1.4; 0.25 -> effect fiebre p 1.8}; \n
+    RandVarEffect fiebre_action = { 0.3 -> effect p_fiebre p 1; 0.45 ->  effect p_fiebre p 1.4; 0.25 -> effect p_fiebre p 1.8}; \n
+    RandVarEffect tos_action = { 0.3 -> effect p_fiebre p 1; 0.45 ->  effect p_fiebre p 1.4; 0.25 -> effect p_fiebre p 1.8}; \n
+    RandVarEffect dc_action = { 0.3 -> effect p_fiebre p 1; 0.45 ->  effect p_fiebre p 1.4; 0.25 -> effect p_fiebre p 1.8}; \n
     Symptom fiebre = { activation_conditions : [new ActivationRule({p_plaqueta : {-0.000000001,16}}, {0,100000000})]; effect_time : 72; repetition : 8; action : fiebre_action;}\n
     Symptom tos = { activation_conditions : [new ActivationRule({p_plaqueta : {-0.000000001,22}}, {0,100000000})]; effect_time : 72; repetition : 8; action : tos_action;}\n
+    RandVarEffect dipirona_simple_action = { 0.7 -> effect p_dolorCabeza p -4.4, p_fiebre p -1; 0.3 -> effect p_fiebre p 0 }\n
+    RandVarEffect dipirona_doble_action = { 0.82 -> effect p_dolorCabeza p -6.8, p_fiebre p -1.5; 0.18 -> effect p_fiebre p 0 }\n
+    RandVarEffect calbamol_action = { 0.4 -> effect p_dolorCabeza p -2, p_fiebre p -0.5, plaqueta p 3; 0.4 -> effect p_dolorCabeza p -3, p_fiebre p -0.5; 0.2 -> effect p_fiebre p 0 }\n
+    RandVarEffect jarabe_action = { 0.75 -> effect tos p -6; 0.25 -> effect tos p 0 }\n
+    RandVarEffect antibiotico_action = { 0.5 -> effect p_fiebre p -1.6, effect tos p -10, effect plaqueta p 2.4; 0.3 -> effect p_fiebre p -1.4, effect tos p -6; 0.2 -> effect p_fiebre p -1, effect plaqueta p 6}\n
+    RandVarEffect plaquetol_action = { 0.5 -> effect plaqueta p 2; 0.3 -> effect plaqueta p 5; 0.2 -> effect plaqueta p 0 }\n
+    Intervention antibiotico = { activation_conditions : [new ActivationRule({p_fiebre : {38, 100}}, {72, 1000000000}), new ActivationRule({p_plaqueta : {-1000000000, 8}}, {24, 1000000000})]; effect_time : 36; repetition : 12; action : antibiotico_action; supply : 7; }\n
+    Intervention dipirona_simple = { activation_conditions : [new ActivationRule({p_fiebre : {37, 100}}, {0, 1000000000}), new ActivationRule({p_dolorCabeza : {10, 1000}}, {0, 1000000000})]; effect_time : 24; repetition : 8; action : dipirona_simple_action; supply : 10; }\n
+    Intervention dipirona_doble = { activation_conditions : [new ActivationRule({p_fiebre : {37, 100}}, {0, 1000000000}), new ActivationRule({p_dolorCabeza : {25, 1000000000}}, {0, 1000000000})]; effect_time : 24; repetition : 8; action : dipirona_doble_action; supply : 10; }\n
+    Intervention calbamol = { activation_conditions : [new ActivationRule({p_fiebre : {37.5, 100}, p_plaqueta : {-1000000000, 30}}, {0, 1000000000}), new ActivationRule({p_dolorCabeza : {15, 1000000000}}, {48, 1000000000}), new ActivationRule({p_plaqueta : {-1000000000, 10}}, {24, 1000000000})]; effect_time : 48; repetition : 6; action : calbamol_action; supply : 8; }\n
+    Intervention plaquetol = { activation_conditions : [new ActivationRule({p_plaqueta : {-1000000000, 10}}, {24, 1000000000})]; effect_time : 24; repetition : 12; action : plaquetol_action; supply : 10; }\n
+    Intervention jarabe = { activation_conditions : [new ActivationRule({p_tos : {10, 1000000000}}, {24, 1000000000})]; effect_time : 48; repetition : 12; action : jarabe_action; supply : 5; }\n
     Symptom dolor_cabeza = { activation_conditions : [new ActivationRule({p_tos : {20,200}}, {0,100000000})]; effect_time : 72; repetition : 4; action : dc_action;}"""
 )
-""" """
+
